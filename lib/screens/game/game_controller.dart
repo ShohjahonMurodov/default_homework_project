043a9a113e20f7b0172a7a22aa432b20ea63f4ea
Shohjahon Murodov <shohjahonmurodov84@gmail.com>
2024-04-17 11:56:42 +0500

    Tugadi :)

--- lib/screens/game/game_controller.dart
+++ lib/screens/game/game_controller.dart
@@ -1,8 +1,34 @@
 import 'dart:math';
-
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:homework/data/models/game_models.dart';
 
 class GameController extends GetxController {
+  final questions = <GameModels>[
+    GameModels(
+      riddle: "Alisherning ismi nima?",
+      result: "alisher",
+    ),
+    GameModels(
+      riddle: "Suhrobning ismi nima?",
+      result: "suhrob",
+    ),
+    GameModels(
+      riddle: "Shohjahonning ismi nima?",
+      result: "shohjahon",
+    ),
+    GameModels(
+      riddle: "Abubakrning ismi nima?",
+      result: "abubakr",
+    ),
+  ].obs;
+
+  final currentIndex = 0.obs;
+  final inputAnswer = "".obs;
+  final errorMessage = "".obs;
+  List<String> shuffledLetters = [];
+
   List<String> alphabet = [
     "a",
     "b",
@@ -18,27 +44,100 @@ class GameController extends GetxController {
     "m",
   ];
 
-  List<String> randomWords = [];
+  @override
+  void onInit() {
+    super.onInit();
+    shuffleLetters();
+  }
 
-  toRandomWord(String riddleAnswer) {
-    for (int i = 0; i < riddleAnswer.length; i++) {
-      randomWords.add(riddleAnswer[i]);
-    }
+  void shuffleLetters() {
+    shuffledLetters = currentAnswer.split('')..shuffle();
 
     Random random = Random();
 
     for (int i = 0; i < 5; i++) {
       int randomIndex = random.nextInt(alphabet.length);
       String randomLetter = alphabet[randomIndex];
-      randomWords.add(randomLetter);
+      shuffledLetters.add(randomLetter);
     }
-    randomWords.shuffle();
+    shuffledLetters.shuffle();
+    inputAnswer.value = "";
+    errorMessage.value = "";
   }
 
-  toCheck(String riddleAnswer, String answer) {
-    if (riddleAnswer == answer) {
-      return true;
+  String get currentQuestion => questions[currentIndex.value].riddle;
+
+  String get currentAnswer => questions[currentIndex.value].result;
+
+  void addLetter(String letter) {
+    if (inputAnswer.value.length < currentAnswer.length) {
+      inputAnswer.value += letter;
+      checkAnswer();
     }
-    return false;
+  }
+
+  void checkAnswer() {
+    if (inputAnswer.value == currentAnswer) {
+      errorMessage.value = "To'g'ri!";
+      nextQuestion();
+    } else if (inputAnswer.value.length == currentAnswer.length) {
+      errorMessage.value = "Xato javob";
+      inputAnswer.value = "";
+    }
+  }
+
+  void nextQuestion() {
+    if (currentIndex.value < questions.length - 1) {
+      currentIndex.value++;
+      shuffleLetters();
+    } else {
+      Get.dialog(
+        AlertDialog(
+          title: Text(
+            'GetX Dialog',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30.sp,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          content: Text(
+            'This is a GetX dialog.',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24.sp,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          actions: [
+            Center(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.w, vertical: 11.h),
+                    backgroundColor: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.r),
+                    )),
+                onPressed: () {
+                  currentIndex.value = 0;
+                  inputAnswer.value = "";
+                  errorMessage.value = "";
+                  Get.back();
+                },
+                child: Text(
+                  "Restart",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.sp,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
--- lib/screens/game/view/game_screen.dart
+++ lib/screens/game/view/game_screen.dart
@@ -1,145 +1,127 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:homework/data/models/game_models.dart';
 import 'package:homework/screens/game/game_controller.dart';
 import 'package:homework/utils/app_images.dart';
-import 'package:homework/utils/size_utils.dart';
 
-class GameScreen extends StatefulWidget {
-  const GameScreen({super.key});
-
-  @override
-  State<GameScreen> createState() => _GameScreenState();
-}
-
-class _GameScreenState extends State<GameScreen> {
-  List<String> randomWords = [];
-
-  int activeRiddle = 0;
-
-  final controller = Get.put(GameController());
-
-  String answer = "";
+class GameScreen extends StatelessWidget {
+  const GameScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    width = MediaQuery.of(context).size.width;
-    height = MediaQuery.of(context).size.height;
-
-    Widget alphabetItems({required String title}) {
-      return Container(
-        margin: EdgeInsets.only(right: 10.w, bottom: 10.h),
-        width: 50.w,
-        height: 60.h,
-        child: TextButton(
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.deepPurple,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.r),
-            ),
-          ),
-          onPressed: () {
-            answer += title;
-            if (answer.length == riddles[activeRiddle].result.length) {
-              if (answer == riddles[activeRiddle].result) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Great"),
-                  ),
-                );
-                activeRiddle++;
-                answer = "";
-                setState(() {});
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Bad"),
-                  ),
-                );
-              }
-            }
-          },
-          child: Center(
-            child: Text(
-              title,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.sp,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-
-    controller.toRandomWord(riddles[activeRiddle].result);
+    final GameController controller = Get.put(GameController());
 
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            AppImages.images,
-            height: double.infinity,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                riddles[activeRiddle].riddle,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.sp,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              50.getH(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Obx(
+        () => Stack(
+          children: [
+            // Background image
+            Image.asset(
+              AppImages.images,
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ...List.generate(
-                    riddles[activeRiddle].result.length,
-                    (index) => Container(
-                      width: 50.w,
-                      height: 60.h,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.r),
-                        border: Border.all(
-                          width: 3.w,
-                          color: Colors.deepPurple,
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          index < answer.length ? answer[index] : "",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 22.sp,
-                            fontWeight: FontWeight.w500,
+                  // Display the current riddle
+                  Text(
+                    textAlign: TextAlign.center,
+                    controller.currentQuestion,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 32.sp,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  // Display the answer spaces
+                  Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      ...List.generate(
+                        controller.currentAnswer.length,
+                        (index) => Container(
+                          margin: EdgeInsets.only(right: 10.w, bottom: 10.h),
+                          width: 50,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: index < controller.inputAnswer.value.length
+                                ? Colors.deepPurple
+                                : Colors.transparent,
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                              width: 3,
+                              color: Colors.deepPurple,
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              index < controller.inputAnswer.value.length
+                                  ? controller.inputAnswer.value[index]
+                                  : "",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  // Display the random alphabet buttons
+                  Wrap(
+                    alignment: WrapAlignment.center,
+                    children: List.generate(
+                      controller.shuffledLetters.length,
+                      (index) => alphabetButton(
+                        title: controller.shuffledLetters[index],
+                        onPressed: () => controller
+                            .addLetter(controller.shuffledLetters[index]),
+                      ),
                     ),
                   ),
                 ],
               ),
-              20.getH(),
-              Wrap(
-                children: [
-                  ...List.generate(
-                    controller.randomWords.length,
-                    (index) => alphabetItems(
-                      title: controller.randomWords[index],
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Widget for alphabet buttons
+  Widget alphabetButton({
+    required String title,
+    required VoidCallback onPressed,
+  }) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10, bottom: 10),
+      width: 50,
+      height: 60,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.deepPurple,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
           ),
-        ],
+        ),
+        onPressed: onPressed,
+        child: Center(
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
       ),
     );
   }
