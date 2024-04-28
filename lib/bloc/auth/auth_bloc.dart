122fbc54738e2d7a899a80a222b571a3631cef04
Shohjahon Murodov <shohjahonmurodov84@gmail.com>
2024-04-28 16:34:18 +0500

    Tugadi :)

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
